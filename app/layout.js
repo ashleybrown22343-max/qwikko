3eb56fa6ad35e30257db234317f1c8065c94424e
ashleybrown22343-max <ashleybrown22343@gmail.com>
2026-09-03 01:41:54 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -21,13 +21,19 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
 
-        {/* Adsterra Social Bar (Bottom, non-intrusive) */}
+        {/* Adsterra Social Bar (Bottom Sticky - Non-intrusive) */}
         <Script
           src="https://pl31145407.profitableratecpmnetwork.com/25/62/2a/25622aa3eb2e34686edc45155c84e026.js"
           strategy="afterInteractive"
         />
 
-        {/* Monetag Vignette (Full-screen, only shows when navigating pages - exact type you liked) */}
+        {/* Adsterra 320x50 Banner (Mobile Bottom Banner) */}
+        <Script
+          src="https://pl31157366.profitableratecpmnetwork.com/025fb75007f85066894958fdd706a051/invoke.js"
+          strategy="afterInteractive"
+        />
+
+        {/* Monetag Vignette (Full-screen, ONLY shows when navigating pages) */}
         <Script
           src="https://n6wxm.com/vignette.min.js"
           strategy="afterInteractive"
